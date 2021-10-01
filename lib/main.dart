4e9a9289e3eeb9d12c359c86ff001198340ae329
iwan-tif1B <80775010+iwan-tif1B@users.noreply.github.com>
2021-10-01 00:51:49 -0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,84 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 
-class MyAppBar extends StatelessWidget {
-  // class widhtget yang propertinya imutable artinya nilainya tidak bisa di ubah
-  const MyAppBar({required this.title, Key? key}) : super(key: key);
-
-  // Fields in a Widget subclass are always marked "final".
-
-  final Widget title;
+class LayoutRow extends StatelessWidget {
+  const LayoutRow({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 56.0, // in logical pixels
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: BoxDecoration(color: Colors.blue[500]),
-      // Row is a horizontal, linear layout.
-      child: Row(
-        // <Widget> is the type of items in the list.
-        children: [
-          const IconButton(// create ikon botton
-            icon: Icon(Icons.menu),
-            tooltip: 'Navigation menu',
-            onPressed: null, // null disables the button
-          ),
-          // Expanded expands its child
-          // to fill the available space.
-          Expanded(
-            child: title,
-          ),
-          const IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class MyScaffold extends StatelessWidget {
-  const MyScaffold({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    // Material is a conceptual piece
-    // of paper on which the UI appears.
-    return Material(
-      // Column is a vertical, linear layout.
-      child: Column(
-        children: [
-          MyAppBar(
-            title: Text(
-              'Example title',//untuk judul aplikasi kita
-              style: Theme.of(context) //
-                  .primaryTextTheme
-                  .headline6,
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              flexibleSpace: SafeArea(
+                  // ignore: avoid_unnecessary_containers
+                  child: Container(
+                child: Column(
+                  children: [
+                    Row(
+                      children: const [
+                        IconButton(
+                          icon: Icon(Icons.menu),
+                          tooltip: 'Navigation menu',
+                          onPressed: null, // null disables the button
+                        ),
+                        Spacer(),
+                        Text(
+                          'Kantin Politeknik Kampar',
+                          textAlign: TextAlign.center,
+                        ),
+                        Spacer()
+                      ],
+                    )
+                  ],
                 ),
-              ),
-              const Expanded(
-                child: Center(
-                  child: Text('Hello, world!'),//isi tulisan untuk body
+              )),
             ),
-          ),
-        ],
-      ),
-    );
+            // ignore: avoid_unnecessary_containers
+            body: Container(
+              child: Column(
+                children: [
+                  Row(
+                    children: const [
+                      Text(
+                        'kolom Pertama baris Pertama',
+                        textAlign: TextAlign.left,
+                      ),
+                      Spacer(),
+                      Text(
+                        'kolom kedua baris Pertama',
+                        textAlign: TextAlign.center,
+                      ),
+                      Spacer(),
+                      Text(
+                        'kolom Ketiga baris Pertama',
+                        textAlign: TextAlign.right,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: const [
+                      Text(
+                        'kolom Pertama baris kedua',
+                        textAlign: TextAlign.left,
+                      ),
+                      Spacer(),
+                      Text(
+                        'kolom kedua baris kedua',
+                        textAlign: TextAlign.center,
+                      ),
+                      Spacer(),
+                      Text(
+                        'kolom ketiga baris kedua',
+                        textAlign: TextAlign.right,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )));
   }
 }
 
 void main() {
-  runApp(
-    const MaterialApp(
-      title: 'My app', // used by the OS task switcher
-      home: SafeArea(
-        child: MyScaffold(),
-      ),
-    ),
-  );
+  runApp(LayoutRow());
 }
